f389166da9b92cb634391994b868d4b97173d729
senemis69 <149548660+senemis69@users.noreply.github.com>
2025-10-18 11:36:43 +0700

    Add files via upload

---
+++ lembar kerja 4/mesin.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+df = pd.read_csv("kelulusan_mahasiswa.csv")
+print(df.info())
+print(df.head())
+
+print(df.isnull().sum())
+df = df.drop_duplicates()
+
+
+sns.boxplot(x=df['IPK'])
+
+print(df.describe())
+sns.histplot(df['IPK'], bins=10, kde=True)
+sns.scatterplot(x='IPK', y='Waktu_Belajar_Jam', data=df, hue='Lulus')
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+
+df['Rasio_Absensi'] = df['Jumlah_Absensi'] / 14
+df['IPK_x_Study'] = df['IPK'] * df['Waktu_Belajar_Jam']
+df.to_csv("processed_kelulusan.csv", index=False)
+
+
+
+X = df.drop('Lulus', axis=1)
+y = df['Lulus']
+
+X_train, X_temp, y_train, y_temp = train_test_split(
+    X, y, test_size=0.3, stratify=y, random_state=42)
+
+X_val, X_test, y_val, y_test = train_test_split(
+    X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=42)
+
+print(X_train.shape, X_val.shape, X_test.shape)
